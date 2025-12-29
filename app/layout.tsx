e603dfa3d1107c4c912f3125dbe8439ad367346c
Titan Apriliyan N.A <110273458+titanaprilian@users.noreply.github.com>
2025-12-29 15:57:55 +0700

    fix: add session guard in layout (#126)

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ClientOnlyTopLoader } from "@components/ClientOnlyTopLoader";
 import { PrimeReactProvider } from "primereact/api";
 import { AuthProvider } from "@features/auth/context/AuthProvider";
 import ToastProvider from "../components/ToastProvider";
+import { SessionGuard } from "@features/auth/context/SessionGuard";
 
 const inter = Inter({ subsets: ["latin"] });
 const robotoMono = Roboto_Mono({
@@ -31,12 +32,13 @@ export default function RootLayout({
         <AuthProvider>
           <PrimeReactProvider>
             <ClientOnlyTopLoader />
-
-            {/* 1. Global UI Components */}
-            <ToastProvider>
-              {/* 2. The Page Content */}
-              {children}
-            </ToastProvider>
+            <SessionGuard>
+              {/* 1. Global UI Components */}
+              <ToastProvider>
+                {/* 2. The Page Content */}
+                {children}
+              </ToastProvider>
+            </SessionGuard>
           </PrimeReactProvider>
         </AuthProvider>
       </body>
